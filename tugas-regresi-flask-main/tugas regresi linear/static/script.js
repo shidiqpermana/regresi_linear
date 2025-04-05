1109ab338771bbe2ea09f9b292f5327b14813c36
shidiqpermana <159045938+shidiqpermana@users.noreply.github.com>
2025-04-05 12:20:24 +0700

    Delete tugas-regresi-flask-main/tugas regresi linear directory

--- tugas-regresi-flask-main/tugas regresi linear/static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function () {
-        const button = form.querySelector("button");
-        button.innerText = "Memproses...";
-        button.disabled = true;
-    });
-});
